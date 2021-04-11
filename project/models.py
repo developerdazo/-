3b183ced5adcee62d498c8c86fa57a2857cd7935
goodgame <test20601@gmail.com>
2021-04-11 18:02:28 +0900

    ぶりぶり2021/4/11

---
+++ project/models.py
@@ -0,0 +1,10 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True,nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+    name = db.Column(db.String(1000),nullable=False)
+    loginid = db.Column(db.String(1000),nullable=False)
+    birthday = db.Column(db.DateTime,nullable=False)
+    phonenumber = db.Column(db.String(15),nullable=False,unique=True)
